38ea64b3d4ffc6344ad6a026c671362f66696cd4
alhrmthon <107150406+alhrmthon@users.noreply.github.com>
2022-07-06 11:23:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os, wget
 api_id = int(os.environ.get("APP_ID"))
 api_hash = os.environ.get("API_HASH")
 token = os.environ.get("TOKEN")
-app = Client("yt", app_token=token, api_id = api_id, api_hash = api_hash)
+app = Client("yt", bot_token=token, api_id = api_id, api_hash = api_hash)
 
 
 @app.on_message(ay.command("start"))
